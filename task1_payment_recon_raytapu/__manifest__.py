de778a73f6845bba17fda654fa3fcb43404e7862
musfiqbetopia-del <musfiq.betopia@gmail.com>
2026-06-25 13:27:03 +0600

    Add manifest and root init for task1 payment reconciliation module

---
+++ task1_payment_recon_raytapu/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Automatic SO Payment Reconciliation',
+    'version': '19.0.1.0.0',
+    'category': 'Accounting',
+    'summary': 'Auto-reconcile payments to invoices via SO reference in journal item label',
+    'depends': ['account',],
+
+      'data': [
+        # we'll add files here as we build them
+    ],
+    'installable': True,
+    'license': 'LGPL-3',
+}
